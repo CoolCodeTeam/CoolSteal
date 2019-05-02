2195d6c5c6e9803ec91b49998c4570ba801011a7
AntonPriyma <apriyma0@gmail.com>
2019-05-02 09:08:13 +0300

    [UPD] gitignore

---
+++ cmake-build-debug/CMakeFiles/CoolSteal.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anton/Programs/CoolSteal/CoolStealServerApp.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/CoolStealServerApp.cpp.o"
+  "/home/anton/Programs/CoolSteal/PlagiasmChecker.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/PlagiasmChecker.cpp.o"
+  "/home/anton/Programs/CoolSteal/Router.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/Router.cpp.o"
+  "/home/anton/Programs/CoolSteal/main.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/main.cpp.o"
+  "/home/anton/Programs/CoolSteal/methods/CheckLibary.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/methods/CheckLibary.cpp.o"
+  "/home/anton/Programs/CoolSteal/methods/ShingleChecker.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/methods/ShingleChecker.cpp.o"
+  "/home/anton/Programs/CoolSteal/models/DBCommand.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/models/DBCommand.cpp.o"
+  "/home/anton/Programs/CoolSteal/models/PlagiasmResult.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/models/PlagiasmResult.cpp.o"
+  "/home/anton/Programs/CoolSteal/models/Program.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/models/Program.cpp.o"
+  "/home/anton/Programs/CoolSteal/models/Token.cpp" "/home/anton/Programs/CoolSteal/cmake-build-debug/CMakeFiles/CoolSteal.dir/models/Token.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CoolSteal.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoolSteal.dir/main.cpp.o"
+  "CMakeFiles/CoolSteal.dir/models/Program.cpp.o"
+  "CMakeFiles/CoolSteal.dir/models/PlagiasmResult.cpp.o"
+  "CMakeFiles/CoolSteal.dir/models/Token.cpp.o"
+  "CMakeFiles/CoolSteal.dir/models/DBCommand.cpp.o"
+  "CMakeFiles/CoolSteal.dir/CoolStealServerApp.cpp.o"
+  "CMakeFiles/CoolSteal.dir/Router.cpp.o"
+  "CMakeFiles/CoolSteal.dir/PlagiasmChecker.cpp.o"
+  "CMakeFiles/CoolSteal.dir/methods/ShingleChecker.cpp.o"
+  "CMakeFiles/CoolSteal.dir/methods/CheckLibary.cpp.o"
+  "CoolSteal.pdb"
+  "CoolSteal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoolSteal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
