d5460fd5359d4002099d6f2cfa5331273a4c2608
AntonPriyma <apriyma0@gmail.com>
2019-05-04 14:55:27 +0300

    [UPD] Merged Normalize

---
+++ Normalizator/CppNormaizeAlgorithm.cpp
@@ -0,0 +1,69 @@
+//
+// Created by anton on 04.05.19.
+//
+
+#include "CppNormaizeAlgorithm.h"
+#include <algorithm>
+std::string CppNormaizeAlgorithm::run(const Program &program) {
+  std::string sourseCode  = program.getSourseCode();
+  int n = sourseCode.length();
+  std::string res;
+  int i = 0, j = -1;
+
+  bool spaceFound = false;
+
+  while (++j < n && sourseCode[j] == ' ');
+
+  while (j < n)
+  {
+    if (sourseCode[j] != ' ')
+    {
+      if ((sourseCode[j] == '.' || sourseCode[j] == ',' ||
+          sourseCode[j] == '?' || sourseCode[j] == ';') && i - 1 >= 0 &&
+          sourseCode[i - 1] == ' ')
+        sourseCode[i - 1] = sourseCode[j++];
+
+      else
+        sourseCode[i++] = sourseCode[j++];
+
+      spaceFound = false;
+    }
+
+    else if (sourseCode[j++] == ' ')
+    {
+      if (!spaceFound)
+      {
+        sourseCode[i++] = ' ';
+        spaceFound = true;
+      }
+    }
+  }
+  if (i <= 1)
+    sourseCode.erase(sourseCode.begin() + i, sourseCode.end());
+  else
+    sourseCode.erase(sourseCode.begin() + i - 1, sourseCode.end());
+
+  bool singleCommentFlag = false;
+  bool multiplyCommentFlag = false;
+
+  n = sourseCode.length();
+  for (int i=0; i<n; i++)
+  {
+    if (singleCommentFlag && sourseCode[i] == '\n')
+      singleCommentFlag = false;
+
+    else if  (multiplyCommentFlag && sourseCode[i] == '*' && sourseCode[i+1] == '/')
+      multiplyCommentFlag = false,  i++;
+
+    else if (singleCommentFlag || multiplyCommentFlag)
+      continue;
+
+    else if (sourseCode[i] == '/' && sourseCode[i+1] == '/')
+      singleCommentFlag = true, i++;
+    else if (sourseCode[i] == '/' && sourseCode[i+1] == '*')
+      multiplyCommentFlag = true,  i++;
+    else  res += sourseCode[i];
+  }
+  return res;
+
+}
---
+++ Normalizator/Normalizator.cpp
@@ -0,0 +1,15 @@
+//
+// Created by anton on 04.05.19.
+//
+
+#include "Normalizator.h"
+#include "CppNormaizeAlgorithm.h"
+std::string Normalizator::normalize(const Program &program) {
+  algorithm = configureNormalizer(program.getLang());
+  return algorithm->run(program);
+}
+NormalizeAlgorithm* Normalizator::configureNormalizer(std::string lang) {
+  if (strcmp(lang.c_str(),"CPP") == 0){
+    return new CppNormaizeAlgorithm();
+  }
+}
